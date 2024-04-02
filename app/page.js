c2ca5aa0a89378b88437f86b00441c5e76691adc
Heather Hazell <74628856+HeatherHazell@users.noreply.github.com>
2024-04-02 13:17:54 -0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'next/link';
+
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Empower Your Plants</h1>
+            <h2>This is the landing page.</h2>
+            <h3>
+                <Link href="/products">
+                    Go to Products Page
+                </Link>
+            </h3>
+        </div>
+    );
+};
+
+export default IndexPage;
