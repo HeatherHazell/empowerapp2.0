00a57c263b0d14218806584df40821cef2dbacb3
Heather Hazell <74628856+HeatherHazell@users.noreply.github.com>
2024-04-04 14:15:51 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -23,7 +23,7 @@ transpileClientSDK: true,
 // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers. (increases server load)
 // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
 // side errors will fail.
-tunnelRoute: "https://o88872.ingest.us.sentry.io/api/2aaa3ad3ba5ea01c289d2297fd91be88/envelope/?sentry_key=2aaa3ad3ba5ea01c289d2297fd91be88",
+// tunnelRoute: "https://o88872.ingest.us.sentry.io/api/2aaa3ad3ba5ea01c289d2297fd91be88/envelope/?sentry_key=2aaa3ad3ba5ea01c289d2297fd91be88/",
 
 // Hides source maps from generated client bundles
 hideSourceMaps: true,
